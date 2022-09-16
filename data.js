3f4193d7dd18bc03fd93c7e76dbc346c53281dcd
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-16 09:17:35 +0300

    add javascript

---
+++ data.js
@@ -0,0 +1,36 @@
+const data = [{
+  image: "Assets/koenigsegg-logo.png",
+  name: "Koenigsegg",
+  description: "Koenigsegg is a Swedish avante-grade megacar and innovative technology company",
+  bio: "A record-breaking tradition courses through our history - the result of pushing the boundaries of automotive technology to achieve higher standards in ultimate performance."
+},
+{
+  image: "Assets/Ferrari-Logo.svg.png",
+  name: "Scuderia Ferrari",
+  description: "People of excellence enable us to take on the challenges of the world's markets and tracks - and win.",
+  bio: "Born of the spirit of racing, Ferrari epitomises the power of a lifelong passion and the beauty of limitless human achievement, creating timelss icons for a changing world."
+},
+{
+  image: "Assets/bugatti-logo.png",
+  name: "Bugatti",
+  description: "The success of the BUGATTI brand is underpinned by honesty and integrity on the part of all our employees.",
+  bio: "Carlo Bugatti (1856 - 1940) was an internationally renowned, award-winning artist, designer and silversmith. His furniture was especially famous."
+},
+{
+  image: "Assets/mclaren-logo.png",
+  name: "McLaren",
+  description: "McLaren Group is globally renowned as one of the world’s most illustrious high-technology brands.",
+  bio: "Since the formation of McLaren Racing in 1963, McLaren has been pioneering and innovating in the competitive world of Formula 1, forging a formidable reputation which has seen the racing team win 20 World Championships and over 180 races."
+},
+{
+  image: "Assets/pagani-logo.png",
+  name: "Pagani",
+  description: "That of Horacio Pagani is the great adventure of an art and engineering crusade.",
+  bio: "What it is in the making, which only exists as a spark that hasn't found its form yet; it’s what can only happen through an intuition. The future is the breeze that is yet to blow, the wind that only the genius can catch and transform into a project, like the great Italian masters who have always inspired the myth of Pagani."
+},
+{
+  image: "Assets/porsche-logo.png",
+  name: "Porsche",
+  description: "In the beginning, I looked around and could not find quite the car I dreamed of. So I decided to build it myself",
+  bio: "Porsche has defined its goals based on the four stakeholder dimensions of customers, society, employees and investors. In keeping with this vision, the company aims to be the most recognised brand in the world and one that particularly excites its customers. "
+  }]
